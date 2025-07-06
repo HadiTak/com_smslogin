3f8a72703ab76d0a2ca282e77ed7da11be7a2d75
HadiTak <HadiTak@users.noreply.github.com>
2025-07-06 13:38:06 +0330

    Create config.php

---
+++ admin/config/config.php
@@ -0,0 +1,14 @@
+<?php
+defined('_JEXEC') or die;
+
+return [
+    'sms_api_key' => 'OWYxMWE1OTQtYjY0ZC00ZjVjLTlhMjgtMzkzZmM3MzQyOTYzNGE0ZDM4MDNiZTM4YjQ4OGY1YWI4ZjBjYmQ4MmUzZDM=',
+    'sms_pattern_code' => 'ccuft7jdzc2ynw6',
+    'sms_originator' => '+983000505',
+    'sms_variable' => 'code',
+    'sms_interval' => 10, // ثانیه
+    'max_attempts' => 3,
+    'lock_duration' => 600, // 10 دقیقه
+    'log_path' => '/administrator/smslog.txt',
+    'site_domain' => 'biral.ir'
+];
